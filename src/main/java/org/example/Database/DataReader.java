1bf0940e88ac6f0013b933c63c92ebeb06e9fb26
Washington Ferreira <35137108+Batcleta@users.noreply.github.com>
2023-02-08 11:56:58 -0300

    teste bem sucedido de query por url

--- src/main/java/org/example/Database/DataReader.java
+++ src/main/java/org/example/Database/DataReader.java
@@ -5,7 +5,10 @@ import org.example.Models.Estatistica;
 import org.example.Models.Gol;
 import org.example.Models.Partida;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
@@ -34,22 +37,33 @@ public class DataReader implements IDataReader {
 
     public List<Partida> readMatches() {
         List<Partida> matches = new ArrayList<>();
+
+        List<String[]> data = null;
+
         try {
-            List<String[]> data = Files.lines(Paths.get(matchUrl))
+            URL url = new URL("https://raw.githubusercontent.com/vconceicao/ada_brasileirao_dataset/master/campeonato-brasileiro-full.csv");
+            BufferedReader reader = new BufferedReader(new InputStreamReader(url.openStream()));
+            data = reader.lines()
                     .map(line -> line.split(","))
                     .map(array -> Arrays.stream(array)
                             .map(str -> str.replaceAll("^\"|\"$", ""))
                             .toArray(String[]::new))
                     .collect(Collectors.toList());
+            reader.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        if (data != null) {
             data.remove(0);
 
             for (String[] match : data) {
                 matches.add(objectFactory.createMatch(match));
             }
-            return matches;
-        } catch (Exception ex) {
-            return matches;
         }
+
+        return matches;
     }
 
     public List<Cartao> readCards() {
