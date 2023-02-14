768108d661e48f06f9828c04514cd4cc6e61b05e
Batcleta <35137108+Batcleta@users.noreply.github.com>
2023-02-13 22:20:14 -0300

    refatorando para stream, times com mais vitórias e estados com menos jogos

--- src/main/java/org/example/Controller/EstadoComMenosJogos.java
+++ src/main/java/org/example/Controller/EstadoComMenosJogos.java
@@ -1,11 +1,9 @@
 package org.example.Controller;
 
+import org.example.Models.Gol;
 import org.example.Models.Partida;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class EstadoComMenosJogos {
@@ -20,41 +18,54 @@ public class EstadoComMenosJogos {
             System.out.println("Não foram encontrados nenhum resultado");
             return;
         }
-        Map<String, Integer> partidasPorEstado = new HashMap<>();
 
-        for (Partida partida : partidasDoAno) {
-            String estado = partida.getMandanteEstado();
-            if (estado != null && !estado.equalsIgnoreCase("-")) {
-                partidasPorEstado.put(estado, partidasPorEstado.getOrDefault(estado, 0) + 1);
-            }
-        }
+        Map<String, Long> jogosPorEstado = partidasDoAno.stream()
+                .filter(partida -> partida.getMandanteEstado() != null && !partida.getMandanteEstado().equalsIgnoreCase("-"))
+                .collect(Collectors.groupingBy(Partida::getMandanteEstado, Collectors.counting()));
 
-//        for (Map.Entry<String, Integer> entry : partidasPorEstado.entrySet()) {
-//            System.out.println("Key: " + entry.getKey() + " Value: " + entry.getValue());
-//        }
+        String estadosComMenosJogos = jogosPorEstado.entrySet().stream()
+                .filter(entry -> entry.getValue() == Collections.min(jogosPorEstado.values()))
+                .map(entry -> "Estado: " + entry.getKey() + ", com: " + entry.getValue() + " jogos.\n")
+                .collect(Collectors.joining(""));
 
-        int minWins = Integer.MAX_VALUE;
-        List<String> pioresEstados = new ArrayList<>();
-
-        for (Map.Entry<String, Integer> time : partidasPorEstado.entrySet()) {
-            if (time.getValue() < minWins) {
-                minWins = time.getValue();
-                pioresEstados.clear();
-                pioresEstados.add("Estado: " + time.getKey() + ", com: " + time.getValue() + " jogos.");
-            } else if (time.getValue() == minWins) {
-                pioresEstados.add("Estado: " + time.getKey() + ", com: " + time.getValue() + " jogos.");
-            }
-        }
-
-        if (pioresEstados.size() > 1) {
-            System.out.print("\nFORAM " + pioresEstados.size() + " ESTADOS COM MENOS JOGOS\n\n");
-        } else {
-            System.out.print("ESTADO COM MENOS JOGOS\n");
-        }
-
-        pioresEstados.forEach(System.out::println);
-
+        System.out.println("=== Estados com menos jogos ===\n" + estadosComMenosJogos);
         System.out.print("\n\n");
+
+//        Map<String, Integer> partidasPorEstado = new HashMap<>();
+//
+//        for (Partida partida : partidasDoAno) {
+//            String estado = partida.getMandanteEstado();
+//            if (estado != null && !estado.equalsIgnoreCase("-")) {
+//                partidasPorEstado.put(estado, partidasPorEstado.getOrDefault(estado, 0) + 1);
+//            }
+//        }
+//
+////        for (Map.Entry<String, Integer> entry : partidasPorEstado.entrySet()) {
+////            System.out.println("Key: " + entry.getKey() + " Value: " + entry.getValue());
+////        }
+//
+//        int minWins = Integer.MAX_VALUE;
+//        List<String> pioresEstados = new ArrayList<>();
+//
+//        for (Map.Entry<String, Integer> time : partidasPorEstado.entrySet()) {
+//            if (time.getValue() < minWins) {
+//                minWins = time.getValue();
+//                pioresEstados.clear();
+//                pioresEstados.add("Estado: " + time.getKey() + ", com: " + time.getValue() + " jogos.");
+//            } else if (time.getValue() == minWins) {
+//                pioresEstados.add("Estado: " + time.getKey() + ", com: " + time.getValue() + " jogos.");
+//            }
+//        }
+//
+//        if (pioresEstados.size() > 1) {
+//            System.out.print("\nFORAM " + pioresEstados.size() + " ESTADOS COM MENOS JOGOS\n\n");
+//        } else {
+//            System.out.print("ESTADO COM MENOS JOGOS\n");
+//        }
+//
+//        pioresEstados.forEach(System.out::println);
+//
+//        System.out.print("\n\n");
     }
 
 }
--- src/main/java/org/example/Controller/TimeDoAno.java
+++ src/main/java/org/example/Controller/TimeDoAno.java
@@ -2,10 +2,7 @@ package org.example.Controller;
 
 import org.example.Models.Partida;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class TimeDoAno {
@@ -17,36 +14,50 @@ public class TimeDoAno {
             System.out.println("Não foram encontrados nenhum resultado");
             return;
         }
-        Map<String, Integer> vitoriasPorTime = new HashMap<>();
 
-        for (Partida partida : partidasDoAno) {
-            String vencedor = partida.getVencedor();
-            if (vencedor != null && !vencedor.equalsIgnoreCase("-")) {
-                vitoriasPorTime.put(vencedor, vitoriasPorTime.getOrDefault(vencedor, 0) + 1);
-            }
-        }
+        Map<String, Long> vitoriasPorTime = partidasDoAno.stream()
+                .filter(partida -> partida.getVencedor() != null && !partida.getVencedor().equalsIgnoreCase("-"))
+                .collect(Collectors.groupingBy(Partida::getVencedor, Collectors.counting()));
 
-        int maxWins = 0;
-        List<String> melhoresTimes = new ArrayList<>();
+        String melhoresTimes = vitoriasPorTime.entrySet().stream()
+                .filter(entry -> entry.getValue() == Collections.min(vitoriasPorTime.values()))
+                .map(entry -> "Time: " + entry.getKey() + ", com: " + entry.getValue() + " vitórias.\n")
+                .collect(Collectors.joining(""));
 
-        for (Map.Entry<String, Integer> time : vitoriasPorTime.entrySet()) {
-            if (time.getValue() > maxWins) {
-                maxWins = time.getValue();
-                melhoresTimes.clear();
-                melhoresTimes.add("Time: " + time.getKey() + ", com: " + time.getValue() + " vitórias.");
-            } else if (time.getValue() == maxWins) {
-                melhoresTimes.add("Time: " + time.getKey() + ", com: " + time.getValue() + " vitórias.");
-            }
-        }
-
-        if (melhoresTimes.size() > 1) {
-            System.out.print("\nFORAM " + melhoresTimes.size() + " TIMES VITORIOSOS\n\n");
-        } else {
-            System.out.print("TIME VITORIOSO\n");
-        }
-
-        melhoresTimes.forEach(System.out::println);
-
+        System.out.println("=== Times com mais vitórias ===\n" + melhoresTimes);
         System.out.print("\n\n");
+
+
+//        Map<String, Integer> vitoriasPorTime = new HashMap<>();
+//
+//        for (Partida partida : partidasDoAno) {
+//            String vencedor = partida.getVencedor();
+//            if (vencedor != null && !vencedor.equalsIgnoreCase("-")) {
+//                vitoriasPorTime.put(vencedor, vitoriasPorTime.getOrDefault(vencedor, 0) + 1);
+//            }
+//        }
+//
+//        int maxWins = 0;
+//        List<String> melhoresTimes = new ArrayList<>();
+//
+//        for (Map.Entry<String, Integer> time : vitoriasPorTime.entrySet()) {
+//            if (time.getValue() > maxWins) {
+//                maxWins = time.getValue();
+//                melhoresTimes.clear();
+//                melhoresTimes.add("Time: " + time.getKey() + ", com: " + time.getValue() + " vitórias.");
+//            } else if (time.getValue() == maxWins) {
+//                melhoresTimes.add("Time: " + time.getKey() + ", com: " + time.getValue() + " vitórias.");
+//            }
+//        }
+//
+//        if (melhoresTimes.size() > 1) {
+//            System.out.print("\nFORAM " + melhoresTimes.size() + " TIMES VITORIOSOS\n\n");
+//        } else {
+//            System.out.print("TIME VITORIOSO\n");
+//        }
+//
+//        melhoresTimes.forEach(System.out::println);
+//
+//        System.out.print("\n\n");
     }
 }
